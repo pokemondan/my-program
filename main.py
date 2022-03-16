654bf7b0b857333e5b71c7cc2c3dd3fce5845322
Daniel bishop <pokemondan788@gmail.com>
2022-03-16 11:41:56 +0000

    add code

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
-number1 = int(input( Enter a number))
+number1 = int(input(" Enter a number "))
+number2 = int(input(" Enter a number"))
+answer = number1/number2
 
-number2 = int(input( Enter a number))
+
+number1 = int(input(" Enter the length "))
+number2 = int(input(" Enter the length"))
+area = number1*number2
+print (area)
+
